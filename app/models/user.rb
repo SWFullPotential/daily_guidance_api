d85e059912bddff2ee406cb067583735ebc568bb
SWFullPotential <swfullpotential@gmail.com>
2020-10-13 16:44:30 -0600

    updated password minimum length

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
+    validates :username, length: { minimum: 3 }
     validates :email, presence: true
     validates :email, uniqueness: true
 end

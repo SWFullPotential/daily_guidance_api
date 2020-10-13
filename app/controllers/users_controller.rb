2bc34ff7f11186b8f9ba1229fb6eb4882a3e4690
SWFullPotential <swfullpotential@gmail.com>
2020-10-13 16:19:17 -0600

    fixed typo on line 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class Usersontroller < ApplicationController
             else
                 render json: {
                     status: 500, 
-                    errors: ['no users found'
+                    errors: ['no users found']
                 }
             end
     end
